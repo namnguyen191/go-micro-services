e7b11ed729615608fc848bd5a1b5c167ca828c92
NAM NGUYEN <hoangnamnguyen191@gmail.com>
2022-09-04 22:01:28 -0400

    feat: add testing

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -21,13 +21,13 @@ func (app *Config) Authenticate(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	// validate the user against the databse
-	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+	user, err := app.Repo.GetByEmail(requestPayload.Email)
 	if err != nil {
 		app.errorJSON(rw, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	valid, err := user.PasswordMatches(requestPayload.Password)
+	valid, err := app.Repo.PasswordMatches(requestPayload.Password, *user)
 	if err != nil || !valid {
 		app.errorJSON(rw, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
@@ -65,8 +65,7 @@ func (app *Config) logRequest(name, data string) error {
 		return err
 	}
 
-	client := http.Client{}
-	_, err = client.Do(request)
+	_, err = app.Client.Do(request)
 	if err != nil {
 		return err
 	}
